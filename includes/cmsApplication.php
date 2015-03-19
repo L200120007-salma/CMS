024d99cc72d2794b4e3c1a482bee932a477cc86d
unknown <tdsalma@gmail.com>
2015-03-19 10:48:44 +0700

    percobaan 3_menambah file utama sistem cms

--- includes/cmsApplication.php
+++ includes/cmsApplication.php
@@ -6,6 +6,28 @@ class CmsApplication extends CmsBase {
 	//dapat diakses melalui fungsi utama
 	// dalam cms
 	//management halaman web
+	function run (){
+		if (isset($_REQUEST['task']))  { // memastikan variable ini sudah di deklarasi atau ada isinya atau tidak
+	//$_REQUEST[''] merupakan variable built in dalam php, untuk memastikan bahwa di request oleh user atau tidak 
+	 	$task = $_REQUEST['task'];
+	 	switch ($task){
+	 			case 'addcontent':
+	 				$this -> addcontent();
+	 				echo '<hr><h1> fungsi ini menampilkan addcontent';
+	 				break;
+	 			case 'anyothertask':
+	 				$this -> anyothertask();
+	 				echo '<hr><h1> fungsi ini menampilkan anyothertask';
+	 				break;
+	 			default :
+	 				$this -> viewcontent();
+	 				echo '<hr><h1> fungsi ini menampilkan aviewcontent apasaja';
+	 	}
+	}else {
+		$this -> viewcontent();
+	}
+		}
+
 	function addcontent()
 	{
 		echo 'disini akan dibuat fungsi menambah konten';
---
+++ templates/default/css/template.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+	<title>Ini template default dalam cms</title>
+	<link type='text/css' rel="stylesheet" href='<?php echo $this->getCurrentTemplatePath(); ?>CSS/style.css'/>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="header">MyCMS</div>
+		<div class="clear"></div>
+		<div class="sidebar">disini letak teks bagian sisi halaman web</div>
+		<div class="content"><?php echo $this->appOutput(); ?></div>
+		<br><br></div>
+		<div class="clear"></div>
+		<div class="footer">disini bagian footer</div>
+	</div>
+</body>
+</html>
